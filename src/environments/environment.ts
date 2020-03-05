b5ee9c23ba4492e8f8c4dd40615ee3a7b1646fe1
wcallegmt <developer06.gmt@gmail.com>
2020-03-05 14:50:34 -0500

    Fusionando rama devveloper a master!!

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false,
+  production: true,
   URI_API: 'https://sisges.maximogt.com'
 };
 
